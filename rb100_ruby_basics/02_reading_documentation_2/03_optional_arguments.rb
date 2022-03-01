338ae4601900881ed2eee61c82d49a18eba15341
Chanda Tivey <chantivey@icloud.com>
2022-03-01 06:28:18 -0500

    Completed Reading Documentation 2

---
+++ rb100_ruby_basics/02_reading_documentation_2/03_optional_arguments.rb
@@ -0,0 +1,6 @@
+# What will each of the 3 puts statements print?
+
+s = 'abc def ghi,jkl mno pqr,stu vwx yz'
+puts s.split.inspect            # ["abc", "def", "ghi,jkl", "mno", "pqr,stu", "vwx", "yz"]
+puts s.split(',').inspect       # ["abc def ghi", "jkl mno pqr", "stu vwx yz"]
+puts s.split(',', 2).inspect    # ["abc def ghi", "jkl mno pqr,stu vwx yz"]
