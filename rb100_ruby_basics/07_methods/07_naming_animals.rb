9e102d145d45765ece1678ef27e7d3f7230c1092
Chanda Tivey <chantivey@icloud.com>
2022-03-21 20:25:53 -0400

    Completed Methods exercises

---
+++ rb100_ruby_basics/07_methods/07_naming_animals.rb
@@ -0,0 +1,15 @@
+# Run the code as shown below, and take note of any error messages. Based on what
+# the error messages are telling you, update the relevant method definitions and
+# method invocations as necessary so that the names are printed as shown below. 
+
+def dog(name)
+  return name
+end
+
+def cat(name)
+  return name
+end
+
+puts "The dog's name is #{dog('Spot')}."
+puts "The cat's name is #{cat('Ginger')}."
+
